ed95d8ce9fdeb2debedd88806913df561ca43d05
Meagan Gardner <meagan_gardner@hotmail.com>
2025-07-31 21:38:33 -0700

    updated docs

--- DSCI_511_py-prog_students-test/solutions/worksheet2/untidy-food-consumption.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv"
-df = pd.read_csv(url)
-
-df_melt = df.melt(id_vars=['country', 'food_category'], var_name='metrics', value_name='measurements')
-df_melt.to_csv("data/food_consumption2.csv", index=False)
